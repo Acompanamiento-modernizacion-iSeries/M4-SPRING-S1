2058d0e8a8876d7bafcc3d12568bb57a6f29ba1a
Edwin Rivera <edwriver@bancolombia.com.co>
2024-10-07 16:30:47 -0500

    Taller 1: Entrega - Api Manejo Cuenta
    Entrega del proyecto 1 donde se maneja unos enpoints con un set de facturas predeterminadas

---
+++ src/main/java/com/coban/apimanejocuenta/ApiManejoCuentaApplication.java
@@ -0,0 +1,13 @@
+package com.coban.apimanejocuenta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiManejoCuentaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiManejoCuentaApplication.class, args);
+    }
+
+}
